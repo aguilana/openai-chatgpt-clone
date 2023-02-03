a234c739cc1440bda0ae04f02b96732febbb148a
aguilana <naguila.mandich@gmail.com>
2023-02-03 08:25:23 -0500

    added starter files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    
+  entry: ["./src/index.js"],
+
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js",
+  },
+
+  context: __dirname,
+
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    ],
+  },
+};
